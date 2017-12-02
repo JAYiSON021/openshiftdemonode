1a3bd074204b5e83246c4e7056669b5a70367d71
Jayson San Agustin <sanagustinjayson@gmail.com>
2017-12-02 09:47:23 +0800

    Commit 4

--- config/index.js
+++ config/index.js
@@ -5,8 +5,12 @@ Object.defineProperty(exports, "__esModule", {
 });
 // sets of configs that are advised not to commit on public repos
 // or must be in environmental vars.
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 exports.default = {
-    "port": 3002,
+    
+    "port": port,
     "localMongoUrl": "mongodb://localhost:27017/gg-platform",
     "mongoUrlProd": "mongodb://62b9ccd95243103bd6e83644b2d11dff:72F86f7c252b888@5a.mongo.evennode.com:27017,5b.mongo.evennode.com:27017/62b9ccd95243103bd6e83644b2d11dff?replicaSet=us-5",
     "bodyLimit": "100kb",
