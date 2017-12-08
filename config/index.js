948eaf06fc8a4e4fdec8fb0dc25e76fe16da5cff
Jayson San Agustin <sanagustinjayson@gmail.com>
2017-12-08 14:10:54 +0800

    Commit 6

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 exports.default = {
     
-    "port": port,
+    "port": process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     "localMongoUrl": "mongodb://localhost:27017/gg-platform",
     "mongoUrlProd": "mongodb://62b9ccd95243103bd6e83644b2d11dff:72F86f7c252b888@5a.mongo.evennode.com:27017,5b.mongo.evennode.com:27017/62b9ccd95243103bd6e83644b2d11dff?replicaSet=us-5",
     "bodyLimit": "100kb",
